1951b85e02f2c13a3577bd13620f440b885dcfd1
Mohit Singh <mohitkumar89800@gmail.com>
2025-09-10 14:20:50 +0530

    chore(repo): move repo root to wp-content and add mu-plugins

---
+++ mu-plugins/university-post-types.php
@@ -0,0 +1,21 @@
+<?php 
+
+function university_post_types() {
+    register_post_type('event', array(
+        'supports' => array('title', 'editor', 'excerpt'),
+        'rewrite' => array('slug' => 'events'),
+        'has_archive' => true,
+        'public' => true,
+        'show_in_rest' => true,
+        'labels' => array (
+            'name' => 'Events',
+            'add_new_item' => 'Add New Event',
+            'edit_item' => 'Edit Events',
+            'all_items' => 'All Events',
+            'singular' => 'Event'
+        ),
+        'menu_icon' => 'dashicons-calendar'
+    ));
+}
+
+add_action('init', 'university_post_types');
